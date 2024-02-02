5f2aab84edc182253362dd1d2a518c1185505b9d
w0ahL <149548505+w0ahL@users.noreply.github.com>
2024-02-01 21:35:42 -0500

    Add emote vaildation

--- moderate.js
+++ moderate.js
@@ -380,6 +380,24 @@ module.exports.moderate = async (realmData) => {
             }
          })
 
+	client.on('emote', async (packet) => {
+		const dbAccount = await accountsModel.findOne({
+               		xuid: packet.xuid
+            	});
+
+            	if (!dbAccount) return;
+
+		if (packet.flags === 'mute_chat') {
+			console.log(`[${dbAccount.xuid}] Bad emote information [T1]`);
+                	client.sendCommand(`kick "${dbAccount.xuid}" Invaild emote information sent. [T1] `, 0)
+		}
+
+		if (!isUUIDv4(packet.emote_id)) {
+			console.log(`[${dbAccount.xuid}] Bad emote information [T2]`);
+               		client.sendCommand(`kick "${dbAccount.xuid}" Invaild emote information sent. [T2] `, 0);
+		}
+	 })
+
          client.on('start_game', async () => {
             console.log(`Joined`);
 
